b839a215844ba30f240e2bcf68553557018bcac4
Pastor Elvis <Pastor Elvis@ACCIFRUITBEARER>
2025-08-21 20:09:33 +0100

    Add project files.

---
+++ SelenuimBasics/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SelenuimBasics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SelenuimBasics")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0134ee26-b20f-4333-bd13-65b1cb6b84a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SelenuimBasics/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace SeleniumBasics
+{
+    public class GoogleSmokeTests
+    {
+        private IWebDriver _driver = null!;
+        private WebDriverWait _wait = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+
+            _driver = new ChromeDriver(options); // Selenium Manager handles driver
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void Can_Open_Google_And_See_Title()
+        {
+            _driver.Navigate().GoToUrl("https://www.google.com");
+            _wait.Until(d => d.Title.Length > 0);
+            Assert.That(_driver.Title, Does.Contain("Google"));
+        }
+    }
+}
